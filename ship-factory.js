21c0b97b5e93c0e61b18a18d11e91b98d22f60fd
cagatay-usta <cagatay_usta@windowslive.com>
2023-06-16 19:11:04 +0300

    add all ship types and relevant tests

--- ship-factory.js
+++ ship-factory.js
@@ -1,10 +1,15 @@
 function createShip(name) {
   const length = (() => {
-    if (name == "destroyer") return 3;
+    if (name == "destroyer" || name == "submarine") return 3;
+    if (name == "patrol boat") return 2;
+    if (name == "battleship") return 4;
+    if (name == "carrier") return 5;
   })();
   let hitCount = 0;
 
-  const hit = () => hitCount++;
+  const hit = () => {
+    if (hitCount < length) hitCount++;
+  }
   const isSunk = () => hitCount >= length;
 
   return { hit, isSunk };
--- ship-factory.test.js
+++ ship-factory.test.js
@@ -12,4 +12,39 @@ describe("ship factory", () => {
     testShip.hit();
     expect(testShip.isSunk()).toBe(true);
   });
+
+  test("destroyer not sunk at 2 hits", () => {
+    const testShip = createShip("destroyer");
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.isSunk()).toBe(false);
+  });
+
+  test("destroyer still sunk at 4 hits", () => {
+    const testShip = createShip("destroyer");
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+    testShip.hit();
+    expect(testShip.isSunk()).toBe(true);
+  });
+
+  test("carrier not sunk at 4 hits", () => {
+    const testCarrier = createShip("carrier");
+    testCarrier.hit();
+    testCarrier.hit();
+    testCarrier.hit();
+    testCarrier.hit();
+    expect(testCarrier.isSunk()).toBe(false);
+  });
+
+  test("carrier sunk at 5 hits", () => {
+    const testCarrier = createShip("carrier");
+    testCarrier.hit();
+    testCarrier.hit();
+    testCarrier.hit();
+    testCarrier.hit();
+    testCarrier.hit();
+    expect(testCarrier.isSunk()).toBe(true);
+  });
 });
