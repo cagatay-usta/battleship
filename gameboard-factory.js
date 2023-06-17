668267d81e644f9d745a70d984fac50cadc33d0c
cagatay-usta <cagatay_usta@windowslive.com>
2023-06-17 23:29:13 +0300

    complete receiveAttack board method

--- gameboard-factory.js
+++ gameboard-factory.js
@@ -1,3 +1,5 @@
+const createShip = require("./ship-factory.js");
+
 function createGameBoard(coordinates) {
   // initiate an empty 10x10 gameboard
   const board = [];
@@ -8,15 +10,24 @@ function createGameBoard(coordinates) {
     }
   }
 
+  const ships = [];
+
   function placeShips(coordinates, board) {
     /*
     function to place the ships on the board. coordinates arg is an array of 
     5 elements each contains the coordinates for a ship. 
     In order: Carrier(C) -> Battleship(B) -> Destroyer(D) -> Submarine(S) -> Patrol Boat(P)
     */
-    const symbols = ["C", "B", "D", "S", "P"];
+    const symbols = [
+      "carrier",
+      "battleship",
+      "destroyer",
+      "submarine",
+      "patrol boat",
+    ];
 
-    for (let i = 0; i < coordinates.length - 1; i++) {
+    for (let i = 0; i < coordinates.length; i++) {
+      ships[symbols[i]] = createShip(symbols[i]);
       coordinates[i].forEach(([x, y]) => {
         board[x][y] = symbols[i];
       });
@@ -25,9 +36,23 @@ function createGameBoard(coordinates) {
 
   placeShips(coordinates, board);
 
-  const receiveAttack = ([x, y]) => {};
+  const receiveAttack = ([x, y]) => {
+    // if already hit, return false
+    if (board[x][y] === "hit") return false;
+    // if miss, return -1
+    if (!board[x][y]) {
+      board[x][y] = "hit";
+      return -1;
+    } else {
+      let shipName = board[x][y];
+      board[x][y] = "hit";
+      ships[shipName].hit();
+      if (ships[shipName].isSunk()) return shipName;
+      return 1;
+    }
+  };
 
-  return { board, receiveAttack };
+  return { board, receiveAttack, ships };
 }
 
 module.exports = createGameBoard;
--- gameboard-factory.test.js
+++ gameboard-factory.test.js
@@ -4,41 +4,71 @@
 
 const createGameBoard = require("./gameboard-factory.js");
 
-describe("board", () => {
-  test("ships placed correctly", () => {
-    const input = [
-      [
-        [0, 0],
-        [0, 1],
-        [0, 2],
-        [0, 3],
-        [0, 4],
-      ],
-      [
-        [1, 0],
-        [1, 1],
-        [1, 2],
-        [1, 3],
-      ],
-      [
-        [2, 0],
-        [2, 1],
-        [2, 2],
-      ],
-      [
-        [3, 0],
-        [3, 1],
-        [3, 2],
-      ],
-      [
-        [4, 0],
-        [4, 1],
-      ],
-    ];
+const input = [
+  [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+    [0, 3],
+    [0, 4],
+  ],
+  [
+    [1, 0],
+    [1, 1],
+    [1, 2],
+    [1, 3],
+  ],
+  [
+    [2, 0],
+    [2, 1],
+    [2, 2],
+  ],
+  [
+    [3, 0],
+    [3, 1],
+    [3, 2],
+  ],
+  [
+    [4, 0],
+    [4, 1],
+  ],
+];
 
+describe("board", () => {
+  test("ships are created", () => {
     const playerBoard = createGameBoard(input);
-    expect(playerBoard.board[2][0]).toBe("D");
-    expect(playerBoard.board[1][0]).toBe("B");
-    expect(playerBoard.board[0][0]).toBe("C");
+    expect(Object.keys(playerBoard.ships).length).toBe(5);
+    expect(Object.keys(playerBoard.ships)[0]).toBe("carrier");
+  });
+
+  test("ships placed correctly", () => {
+    const playerBoard = createGameBoard(input);
+    expect(playerBoard.board[0][0]).toBe("carrier");
+    expect(playerBoard.board[1][0]).toBe("battleship");
+    expect(playerBoard.board[2][0]).toBe("destroyer");
+    expect(playerBoard.board[3][0]).toBe("submarine");
+    expect(playerBoard.board[4][0]).toBe("patrol boat");
+  });
+
+  test("empty shot results in miss", () => {
+    const playerBoard = createGameBoard(input);
+    expect(playerBoard.receiveAttack([5, 4])).toBe(-1);
+  });
+  test("already hit shot results in false", () => {
+    const playerBoard = createGameBoard(input);
+    playerBoard.receiveAttack([5, 4]);
+    expect(playerBoard.receiveAttack([5, 4])).toBe(false);
+  });
+  test("successful shot returns 1", () => {
+    const playerBoard = createGameBoard(input);
+    expect(playerBoard.receiveAttack([0, 0])).toBe(1);
+  });
+  test("sinking shot returns ship name", () => {
+    const playerBoard = createGameBoard(input);
+    playerBoard.receiveAttack([0, 0]);
+    playerBoard.receiveAttack([0, 1]);
+    playerBoard.receiveAttack([0, 2]);
+    playerBoard.receiveAttack([0, 3]);
+    expect(playerBoard.receiveAttack([0, 4])).toBe("carrier");
   });
 });
