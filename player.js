5d869d111d350eef74ed696d16149bcf2be842f6
cagatay-usta <cagatay_usta@windowslive.com>
2023-06-19 21:28:33 +0300

    initialize dommodule

--- player.js
+++ player.js
@@ -6,7 +6,19 @@ class Player {
     this.board = createGameBoard();
   }
   attack(board, coords) {
+    if (!coords) coords = this.computerAttack(board);
     return board.receiveAttack(coords);
+  }
+
+  computerAttack(board) {
+    const coords = [];
+    do {
+      let x = Math.floor(Math.random() * 8);
+      let y = Math.floor(Math.random() * 8);
+      coords.push(x);
+      coords.push(y);
+    } while (board.board[coords]);
+    return coords
   }
 }
 
--- player.test.js
+++ player.test.js
@@ -7,4 +7,17 @@ describe("player", () => {
     player.attack(enemy.board, [0, 0]);
     expect(player.attack(enemy.board, [0, 0])).toBe(false);
   });
+
+  test("shots processed to correct board", () => {
+    const player = new Player();
+    const enemy = new Player();
+    player.attack(enemy.board, [0, 0]);
+    expect(enemy.board.board[0][0]).toBe("hit");
+  });
+
+  test("no coords given makes computer play", () => {
+    const player = new Player("player");
+    const enemy = new Player("computer");
+    expect(enemy.attack(player.board)).not.toBe(false);
+  });
 });
