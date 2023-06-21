d6edaa9c4fbda61c32e2735b8a2616636579ab76
cagatay-usta <cagatay_usta@windowslive.com>
2023-06-21 23:55:11 +0300

    integrate player object with dom grid creation

--- DOMmodule.js
+++ DOMmodule.js
@@ -5,8 +5,10 @@ const enemy = new Player("computer");
 
 function createGrids(playerBoard, player) {
   const gridContainer = document.createElement("div");
+  const board = player.board.board;
+  console.log(player);
   gridContainer.classList.add("grid-container");
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < board.length; i++) {
     const grid = document.createElement("div");
     grid.classList.add("grid");
     gridContainer.appendChild(grid);
@@ -15,7 +17,7 @@ function createGrids(playerBoard, player) {
 }
 const playerBoard = document.querySelector(".player1-board");
 const enemyBoard = document.querySelector(".player2-board");
-createGrids(playerBoard);
-createGrids(enemyBoard);
+createGrids(playerBoard, player);
+createGrids(enemyBoard, enemy);
 
 console.log(player.board.board);
